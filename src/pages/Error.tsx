c0db2690204b94586a175e5a137827f6b0835613
bakuyy <sophieyang12345@gmail.com>
2025-06-10 16:44:10 -0400

    feat: error page with autoredirecting

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const Error = () => {
-  return (
-    <div>
-        Error 404: Page not found
-    </div>
-  )
-}
 
-export default Error
+
+    const navigate = useNavigate()
+    const [seconds, setSeconds] = useState(5)
+
+    useEffect(()=>{
+        const timer = setInterval(()=> {
+            setSeconds((prevSeconds) => {
+                if (prevSeconds <= 1) {
+                    clearInterval(timer)
+                    navigate('/')
+                    return 0
+                }
+                return prevSeconds-1
+            })
+        }, 1000)
+
+        return () => clearInterval(timer)
+
+    }, [navigate])
+
+    return (
+        <div>
+            <div>Page Not Found</div>
+            <div>Redirecting you home in {seconds} seconds...</div>
+        </div>
+    )
+    }
+
+ export default Error
